93b9e553d8eae8bd0ed461f43800ede38f65e313
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-01-20 10:58:19 -0300

    fix

---
+++ front-end/src/main.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.tsx'
+import MeetingChat from './pages/MeetingChat.tsx'
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={'/chat'} />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />
+      },
+      {
+        path: "/chat",
+        element: <MeetingChat />
+      }
+    ]
+  }])
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>,
+)
--- front-end/src/pages/MeetingChat.tsx
+++ front-end/src/pages/MeetingChat.tsx
@@ -1,8 +1,8 @@
-import { User } from "lucide-react";
+import { Search, User } from "lucide-react";
 
 export default function MeetingChat() {
   return (
-    <section className="max-w-screen max-h-screen bg-gradient-to-br from-[#f7f7f7] to-slate-700/40">
+    <section className="max-w-screen max-h-screen bg-gradient-to-br from-[#f7f7f7] to-slate-700/40 flex">
       <header className="w-[170px] h-screen border-r border-black relative">
         <h3>Online:  </h3>
         <div className="w-full flex items-center justify-center p-2 flex-col">
@@ -21,6 +21,16 @@ export default function MeetingChat() {
           </span>
         </div>
       </header>
+      <main className="w-full h-screen relative">
+        <div className="renderMessage">
+
+        </div>
+        <div className="input_message absolute bottom-5 flex w-full justify-center items-center">
+          <div className="flex justify-center items-center">
+            <textarea placeholder="Digite a sua pergunta" className="p-2 w-[600px] h-[200px] rounded-xl" />
+          </div>
+        </div>
+      </main>
     </section>
   )
 }
